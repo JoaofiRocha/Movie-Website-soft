9608df55905aa19166e7bc9043d1334400fd93fd
joaofirocha <joaofi@icloud.com>
2025-07-29 16:09:27 -0300

    changes to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Layout from './Components/Layout';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+    return (<footer>
+        <nav>
+            <section>
+                <h3>Navigation</h3>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/profile">Profile</a></li>
+                </ul>
+            </section>
+
+            <section>
+                <h3>Account</h3>
+                <ul>
+                    <li><a href="/contact">Delete Account</a></li>
+                    <li><a href="/help">Edit Account Data</a></li>
+                </ul>
+            </section>
+
+        </nav>
+
+
+        <aside>
+            <p>Data provided by <a href="https://tmdb.org">TMDB</a></p>
+        </aside>
+
+        <small> &copy; 2025 Movie Website</small>
+    </footer>
+    )
+}
+
+export default Footer;
