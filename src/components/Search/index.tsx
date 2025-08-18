8e9221317f0b3dfea95e7f1fc7fe50dd6bba1d69
joaofirocha <joaofi@icloud.com>
2025-08-18 14:28:52 -0300

    finished search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,61 +5,50 @@ import { fetchMulti } from '../../services/tmdbAPI';
 import { mapTMDBMovies } from '../../services/mappers';
 import { debounce } from 'lodash';
 import { useNavigate } from 'react-router-dom';
-import { useSearchStore } from '../../store/useSearchStore';
 
 interface Props {
-    setContent?: (value: Content[]) => void;
-    setSearch?: (value: string) => void;
-    setPages?: (value: number) => void;
-    contentLimit?: number;
+    initialValue?: string;
     placeholder?: string;
     hasFocus?: boolean;
     isLarge?: boolean;
     type?: "bar" | "search";
-    currentPage?: number;
+    contentLimit?: number;
+    className?: string;
 }
 
 
 
-const Search = ({ setContent, setSearch, setPages, currentPage = 1, placeholder = "", hasFocus, isLarge, type = "bar", contentLimit }: Props) => {
-    const query = useSearchStore((state) => state.query);
-    const setQuery = useSearchStore((state) => state.setQuery);
+const Search = ({ className, initialValue, placeholder = "", hasFocus, isLarge, type = "bar", contentLimit }: Props) => {
+    const [query, setQuery] = useState<string>(initialValue ?? '');
+    
 
-    const [movies, setMovies] = useState<(Content)[]>([]);
+    const [movies, setMovies] = useState<Content[]>([]);
     const inputRef = useRef<HTMLInputElement>(null);
     const [showDropdown, setShowDropdown] = useState(false);
     const navigate = useNavigate();
 
 
-
-
-
-
     useEffect(() => {
         const onDebounceSearch = debounce(async (query: string) => {
-            const data = await fetchMulti(query, currentPage);
+            const data = await fetchMulti(query);
             const res = mapTMDBMovies(data?.results, contentLimit);
             setMovies(res);
-            if (setContent)
-                setContent(res);
-            if (setPages)
-                setPages(data?.total_pages);
         }, 500);
 
-        if (query.trim() === "") {
-            if (setContent)
-                setContent([]);
-            setMovies([]);
-            return;
+        if (type === "search") {
+            if (query.trim() === "") {
+
+                setMovies([]);
+                return;
+            }
+            onDebounceSearch(query)
         }
-
-
-        onDebounceSearch(query)
 
         return () => {
             onDebounceSearch.cancel();
         }
-    }, [query, type, currentPage])
+    }, [query, type])
+
 
     const searchClasses = [
         styles.search,
@@ -70,23 +59,21 @@ const Search = ({ setContent, setSearch, setPages, currentPage = 1, placeholder 
     return (
         <form className={styles.form} onSubmit={(e) => {
             e.preventDefault();
-            if (type === "search" && query.trim() !== "") {
-                navigate('/search', {
-                    state: { query: query.trim() }
-                })
+            if (query.trim() !== "") {
+                navigate(`/search?query=${query.trim()}`);
             }
         }}>
 
             <input
-                className={searchClasses}
+                className={`${searchClasses} ${className}`}
                 value={query}
                 type='search'
                 autoCorrect='off'
                 placeholder={placeholder}
                 onChange={(e) => {
-                    if (type === "bar" && setSearch) {
-                        setSearch(e.target.value);
-                    }
+                    // if (type === "bar" && setSearch) {
+                    //     setSearch(e.target.value);
+                    // }
 
                     setQuery(e.target.value);
 
--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,19 +1,59 @@
 import styles from './styles.module.scss';
 import Search from "../../components/Search";
 import List from '../../components/List/index';
-import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fetchMulti } from '../../services/tmdbAPI';
+import { mapTMDBMovies } from '../../services/mappers';
+import { debounce } from 'lodash';
 
 const SearchPage = () => {
+    const [params] = useSearchParams();
     const [movies, setMovies] = useState<Content[]>([])
     const [pages, setPages] = useState<number>(0);
     const [currentPage, setCurrentPage] = useState<number>(1);
-    
+    const [query, setQuery] = useState<string>(params.get('query')??'');
+
+    const onDebounceSearch = debounce(async (query: string, page: number) => {
+        const data = await fetchMulti(query, page);
+        const res = mapTMDBMovies(data?.results);
+        setPages(data?.total_pages);
+        setMovies(res);
+    }, 500);
+
+    useEffect(() => {
+        const queryParam = params.get('query');
+        if (!queryParam || queryParam.trim() === "") {
+            setMovies([]);
+            return;
+        }
+        setQuery(queryParam)
+        onDebounceSearch(queryParam, 1)
+
+
+        return () => {
+            onDebounceSearch.cancel();
+        }
+    }, [params])
+
+    useEffect(() => {
+        if (query.trim() === "") {
+            setMovies([]);
+            return;
+        }
+        onDebounceSearch(query, currentPage)
+
+
+        return () => {
+            onDebounceSearch.cancel();
+        }
+    }, [currentPage])
+
     return (
         <main className={styles.main}>
-            <Search currentPage={currentPage} setPages={setPages} setContent={setMovies} type={'bar'} placeholder='Search for Movie or Tv...'/>
+            <Search className={styles.bar} initialValue={query} type={'bar'} placeholder='Search for Movie or Tv ...' />
 
-            <List listMovie={movies} hasFilter={false} pageMax={20} numberOfPages={pages} onChangePage={setCurrentPage}/>
+            <List className={styles.list} query={query} listMovie={movies} hasFilter={false} pageMax={20} numberOfPages={pages} onChangePage={setCurrentPage} />
         </main>
     );
 }
